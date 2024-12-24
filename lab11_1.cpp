41bf468aa69a665191b16e26b1880517ed90fd08
Thanik30 <jeng.thanik@gmail.com>
2024-12-24 13:49:37 +0700

    lab11_1.cpp completed

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,18 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+
+using namespace std;
+
+int main (){
+    cout << "Press Enter 3 times to reveal your future.\n";
+    
+    for(int i =0 ; i<3;i++){
+        cin.ignore();
+    }
+    srand(time(0));
+    string grades [] = {"A","B+","B","C+","C","D+","D","F","W"};
+    string grade = grades[rand() % 9];
+    cout << "You will get " << grade << " in this 261102.\n";
+    return 0;
+}
